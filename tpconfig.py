409155f54fa9afba1cb1b86340f2e234a5170c69
anjakefala <anja.kefala@gmail.com>
2017-10-24 01:46:08 +0000

    Update configs

--- tpconfig.py
+++ tpconfig.py
@@ -5,10 +5,11 @@ ircd_serverdesc = "dev server"
 ircd_nick = "_"
 
 real_ircd_server = "localhost"
-real_ircd_port = 6667
+real_ircd_port = 6669
 ircd_sid = "100C"
 
-tpchat_port = 80
+tpchat_domain = "tpchat-url"
+tpchat_port = 8080
 htdocs_path = "."
 log_path = "."
 
